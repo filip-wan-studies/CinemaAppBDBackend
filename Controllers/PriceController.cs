84f5cff52b4218392781a5d63c786e20ed951093
Filip <filip.k.wanat@gmail.com>
2020-01-11 21:01:12 +0100

    Add Price Controller

---
+++ Controllers/PriceController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaAppBackend.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaAppBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PriceController : ControllerBase
+    {
+        private readonly IPriceRepository _repository;
+
+        public PriceController(IPriceRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Object> Get()
+        {
+            return _repository.GetPrices();
+        }
+    }
+}
