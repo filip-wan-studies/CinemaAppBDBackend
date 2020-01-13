871803608d1c32f0ee287e9bbc8252aa0b9a1062
Filip <filip.k.wanat@gmail.com>
2020-01-13 21:29:43 +0100

    Add CORS

--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,11 @@ namespace CinemaAppBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowOrigin",
+                    builder => builder.WithOrigins("http://localhost:3000"));
+            });
             services.AddControllers();
             services.AddDbContext<bazdanContext>(opts => opts.UseMySql(Configuration["ConnectionStrings:CinemaAppDatabase"]));
             services.AddScoped<IFilmRepository, FilmRepository>();
@@ -48,6 +53,8 @@ namespace CinemaAppBackend
             
             app.UseRouting();
 
+            app.UseCors(builder =>
+                builder.WithOrigins("http://localhost:3000"));
             //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
