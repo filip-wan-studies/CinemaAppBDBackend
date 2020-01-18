bfbe29b50e8da0172a88b3c864a0147ba2cf902f
Filip <filip.k.wanat@gmail.com>
2020-01-18 09:42:15 +0100

    Add Reservation Service

---
+++ Interfaces/IReservationRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaAppBackend.Models;
+
+namespace CinemaAppBackend.Interfaces
+{
+    public interface IReservationRepository
+    {
+        Reservation GetReservationById(int id);
+
+        List<Reservation> GetReservationsByEmail(string email);
+
+        Reservation PostReservation(string email);
+
+        Reservation PutReservation(int id);
+    }
+}
---
+++ Services/ReservationRepository.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaAppBackend.Interfaces;
+using CinemaAppBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaAppBackend.Services
+{
+    public class ReservationRepository : IReservationRepository
+    {
+        private readonly bazdanContext _context;
+
+        public ReservationRepository(bazdanContext context)
+        {
+            _context = context;
+        }
+
+        public Reservation GetReservationById(int id)
+        {
+            return _context.Reservations.SingleOrDefault(r => r.IdReservation == id);
+        }
+
+        public List<Reservation> GetReservationsByEmail(string email)
+        {
+            return _context.Reservations.Where(r => r.Email == email).ToList();
+        }
+
+        public Reservation PostReservation(string email)
+        {
+            var t = new Ticket {};
+            var reservation = new Reservation {Email = email, DateSubmission = DateTime.Now};
+            return _context.Reservations.Add(reservation).Entity;
+        }
+
+        public Reservation PutReservation(int id)
+        {
+            var reservation = _context.Reservations.SingleOrDefault(r => r.IdReservation == id);
+            return reservation;
+        }
+    }
+}
