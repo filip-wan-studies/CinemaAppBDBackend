fefada12fb6dfc71973040a9347edf7006e7511c
Filip <filip.k.wanat@gmail.com>
2020-01-26 18:50:47 +0100

    Add Client Repository

---
+++ Interfaces/IClientRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaAppBackend.Models;
+
+namespace CinemaAppBackend.Interfaces
+{
+    public interface IClientRepository
+    {
+        IEnumerable<Client> GetClients();
+        Client GetClient(int id);
+        Client GetClient(string email);
+        Client PostClient(Client client);
+        Client PutClient(Client client);
+    }
+}
---
+++ Services/ClientRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaAppBackend.Interfaces;
+using CinemaAppBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaAppBackend.Services
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly dbContext _context;
+
+        public ClientRepository(dbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Client> GetClients()
+        {
+            var clients = _context.Clients.ToList();
+            foreach (var client in clients)
+            {
+                client.Secret = null;
+            }
+
+            return clients;
+        }
+
+        public Client GetClient(int id)
+        {
+            var client = _context.Clients.FirstOrDefault(c => c.Id == id);
+            if (client == null) return null;
+            client.Secret = null;
+            return client;
+        }
+
+        public Client GetClient(string email)
+        {
+            var client = _context.Clients.FirstOrDefault(c => c.Email == email);
+            if (client == null) return null;
+            client.Secret = null;
+            return client;
+        }
+
+        public Client PostClient(Client client)
+        {
+            var existingClient = GetClient(client.Email);
+            if (existingClient != null) return null;
+            Client newClient = new Client
+            {
+                Email = client.Email, Name = client.Name, PhoneNumber = client.PhoneNumber, Surname = client.Surname,
+                Secret = client.Secret
+            };
+            _context.Clients.Add(newClient);
+            _context.SaveChanges();
+            newClient.Secret = null;
+            return newClient;
+        }
+
+        public Client PutClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
